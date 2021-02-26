34a1d89e71d000f76fc3846e05edd177363e65f0
王进 <wangjin@huimei.com>
2021-02-26 19:00:25 +0800

    '提交代码'

---
+++ fh-wj-shop-eureka/src/test/java/com/fh/FhWjShopEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FhWjShopEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
